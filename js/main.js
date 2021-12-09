efa827116e10f628370c3fa0caadd14a2fdf9f78
Shohruh Sadullaev <mello.me.student@gmail.com>
2021-12-09 12:26:12 +0500

    search added

--- js/main.js
+++ js/main.js
@@ -9,14 +9,37 @@ window.onclick = function(event) {
 }
 
 function classToggle() {
-    const navs = document.querySelectorAll('.navbar-collapse')
-    
+    const navs = document.querySelectorAll('.navbar-collapse');
     navs.forEach(nav => nav.classList.toggle('toggle-show'));
   }
   
   document.querySelector('.navbar-toggler')
     .addEventListener('click', classToggle);
 
+function subClassToggle() {
+    const header = document.querySelectorAll('.sub-header');
+    header.classList.toggle('sub-toggle-show');
+  }
+  
+  document.querySelector('.sub-navbar-toggler')
+    .addEventListener('click', subClassToggle);
+
+function searchFilter() {
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName("li");
+      for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+          }
+      }
+  }
 
 var courses = document.getElementById('courses');
 console.log(courses.innerHTML);
